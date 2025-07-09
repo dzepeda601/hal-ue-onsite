9f04295419b2eacfff0a1061a8b52f827ef544db
dzepeda601 <162488787+dzepeda601@users.noreply.github.com>
2025-07-08 20:12:38 -0700

    Update cards.js
    update js from holland doc authoring page

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,9 +4,9 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
+  const rows = [...block.children];
+  rows.forEach((row) => {
     const li = document.createElement('li');
-    moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
@@ -14,11 +14,22 @@ export default function decorate(block) {
     });
     ul.append(li);
   });
+
+  // Add class based on number of <li> elements
+  const liCount = ul.children.length;
+  const gridClass = `grid-${Math.min(4, liCount)}`; // Ensure no grid class exceeds grid-4
+
+  if (liCount >= 2 && liCount <= 4) {
+    block.classList.add(gridClass);
+  } else {
+    block.classList.remove('grid-2', 'grid-3', 'grid-4');
+  }
+
+
   ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
+    img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '350' }]));
   });
+
   block.textContent = '';
   block.append(ul);
 }
